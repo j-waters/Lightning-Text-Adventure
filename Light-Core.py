983e5f5edd99aae90434e6d1448a6f60d78e34dc
James <james.waters@talktalk.net>
2014-09-29 20:00:15 +0100

    Added Light-Core, general system core containing various basic scripts.

---
+++ Light-Core.py
@@ -0,0 +1,34 @@
+import sys
+from urllib.request import urlretrieve
+
+#####
+#Light-Core, by Lightning3105
+#Contains Several important functions
+#####
+
+def debug(string):
+    #Normal print function, outputting to the console
+    sys.stdout.write(str(string))
+    sys.stdout.write("\n")
+
+def reporthook(blocknum, blocksize, totalsize):
+    #Shows percentage of download
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize: # near the end
+            sys.stderr.write("\n")
+    else: # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+def download(url, name):
+    #Downloads a file from a URL
+    debug("Begin Download")
+    urlretrieve(url, name, reporthook)
+
+def run(func):
+    #Runs a function by name, 'func'
+    globals()[func]()
