b227ada361a969bd014e78959c23f45eeae5b852
James <james.waters@talktalk.net>
2014-09-30 20:04:25 +0100

    Created the actual Legend Of Aiopa game file

---
+++ LegendOfAiopaC1.py
@@ -0,0 +1,8 @@
+#####
+#Chapter One of the main Legend Of Aiopa game
+#####
+
+#####
+#Import the Core modules
+from lightCore import *  # @UnusedWildImport
+from adventureCore import *  # @UnusedWildImport
