c06c4713f7a175719feefdf67f9f1867aba6c062
James <james.waters@talktalk.net>
2014-10-04 16:35:01 +0100

    Defined several pictures

--- LegendOfAiopaC1.py
+++ LegendOfAiopaC1.py
@@ -6,3 +6,10 @@
 #Import the Core modules
 from lightCore import *  # @UnusedWildImport
 from adventureCore import *  # @UnusedWildImport
+
+YOU1 = "Pics/YOU1.png"
+YOU2 = "Pics/YOU2.png"
+Full_Bag = "Pics/Bag Full.png"
+Fight_Symbol = "Pics/Fight.png"
+
+player_picture(YOU1, YOU2)
