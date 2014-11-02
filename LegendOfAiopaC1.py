89d3559d8edd9815d1733b7f6e119aa2cd96a3d9
James <james.waters@talktalk.net>
2014-11-02 16:52:48 +0000

    Added a try and except clause around everything

--- LegendOfAiopaC1.py
+++ LegendOfAiopaC1.py
@@ -24,4 +24,14 @@ def create_character():
 def TN1_R1():
     choices(world.TN1_R1)
 
-firstLoad()
+try:
+    firstLoad()
+except:
+    t = time.strftime("%d %b %Y %H|%M|%S", time.gmtime())
+    erf = open("Crash Reports/Error Report at " + t + ".txt", "w")
+    erf.write("\n" + "=" * 20)
+    erf.write("\nCrash At " + t)
+    erf.write("\n")
+    traceback.print_exc(file=erf)
+    erf.close()
+    easygui.exceptionbox(msg="The Legend Of Aiopa Has Crashed.", title="Crash Report")
