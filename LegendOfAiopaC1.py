1fbcc8a9a386d60cf0b3e4efc23696b6ffc975aa
James <james.waters@talktalk.net>
2014-11-02 17:24:31 +0000

    Added the setting of the players health to the players max health on first load. Also, temp commented out the try/except stuff.

--- LegendOfAiopaC1.py
+++ LegendOfAiopaC1.py
@@ -13,6 +13,7 @@ def firstLoad():
     inventory.load()
     if player.load() == False:
         create_character()
+        player.Health = player.MaxHealth
     if world.load() == False:
         TN1_R1()
 
@@ -24,14 +25,14 @@ def create_character():
 def TN1_R1():
     choices(world.TN1_R1)
 
-try:
-    firstLoad()
-except:
-    t = time.strftime("%d %b %Y %H|%M|%S", time.gmtime())
-    erf = open("Crash Reports/Error Report at " + t + ".txt", "w")
-    erf.write("\n" + "=" * 20)
-    erf.write("\nCrash At " + t)
-    erf.write("\n")
-    traceback.print_exc(file=erf)
-    erf.close()
-    easygui.exceptionbox(msg="The Legend Of Aiopa Has Crashed.", title="Crash Report")
+#try:
+firstLoad()
+#except:
+    #t = time.strftime("%d %b %Y %H.%M.%S", time.gmtime())
+    #erf = open("Crash Reports/Error Report at " + t + ".txt", "w")
+    #erf.write("\n" + "=" * 20)
+    #erf.write("\nCrash At " + t)
+    #erf.write("\n")
+    #traceback.print_exc(file=erf)
+    #erf.close()
+    #easygui.exceptionbox(msg="The Legend Of Aiopa Has Crashed.", title="Crash Report")
