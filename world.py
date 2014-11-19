3740d74f84ea8d7d8da28cda14e88f6c1cab912e
James <james.waters@talktalk.net>
2014-11-19 17:32:26 +0000

    Removed world.time function. Replaced all world.times with turn. Removed all time variables.

--- world.py
+++ world.py
@@ -1,16 +1,8 @@
 from lightCore import get_save
 import os.path
-Minute = 0
-Hour = 0
-Day = 0
-Dayp = ""
-Month = 0
-int(Day)
-int(Minute)
-int(Hour)
-Weather = ""
-
 Location = ""
+Turn = 0
+TurnString = ""
 
 TN1_R1 =[]
 
@@ -18,12 +10,8 @@ def save():
     f = open('Saves/World.save', 'w')
 
     save = {
-    'Minute':Minute,
-    'Hour':Hour,
-    'Day':Day,
-    'Dayp':Dayp,
-    'Month':Month,
-    'Weather':Weather
+    'Location':Location,
+    'Turn':Turn
     }
 
     for key in save.keys():
@@ -42,23 +30,3 @@ def load():
         globals().update(get_save('Saves/DEFAULT_World.save'))
         #save() #TEMPORARY
         return False
-
-def time():
-    if Day % 10 == 1:
-        Dayp = "st"
-    if Day % 10 == 2:
-        Dayp = "nd"
-    if Day % 10 == 3:
-        Dayp = "rd"
-    if Day % 10  == 0:
-        Dayp = "th"
-    if Day % 10 > 3:
-        Dayp = "th"
-
-    TheOut = str(Hour) + ":" + str(Minute) + "0" + " " + str(Day) + str(Dayp) + " of " + str(Month)
-
-    #str(TheOut)
-    return TheOut
-
-
-TN1_R1 = []
