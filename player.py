dd7cc3faa96a6b8e45261d6f2085fef8cf616fc7
James <james.waters@talktalk.net>
2015-07-01 20:49:00 +0100

    Added inventory variables, redone saving to work with objects.

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 from lightCore import get_save
 import os.path
+import pickle
+
 Name = ""
 Age = 0
 Health = 0
@@ -21,43 +23,19 @@ Magic = 0
 Spells = []
 Mana = 0
 MaxMana = 0
+InvContents = []
+InvSize = 0
+Money = 0
 
 def save():
     f = open('Saves/Player.save', 'w')
-    save = {
-    'Name':Name,
-    'Age':Age,
-    'Health':Health,
-    'Health':Health,
-    'Helm':Helm,
-    'Shirt':Shirt,
-    'Boot':Boot,
-    'Knowledge':Knowledge,
-    'Karma':Karma,
-    'Xp':Xp,
-    'Xpl':Xpl,
-    'Xpn':Xpn,
-    'Picture':Picture,
-    'Luck':Luck,
-    'Speed':Speed,
-    'Strength':Strength,
-    'Magic':Magic,
-    'Spells':Spells,
-    'Mana':Mana,
-    'MaxMana':MaxMana
-    }
 
-    for key in save.keys():
-        if type(save[key]) == str:
-            item = key + " = '" + save[key] + "'\n"
-        else:
-            item = key + " = " + str(save[key]) + "\n"
-        f.write(item)
-    f.close
+    pickle.dump(globals(), f)
 
 def load():
     if os.path.isfile('Saves/Player.save'):
-        globals().update(get_save('Saves/Player.save'))
+        f = open('Saves/Player.save', 'r')
+        pickle.load(f)
         return True
     else:
         globals().update(get_save('Saves/DEFAULT_Player.save'))
